de051960e8c89297eb05542fd083808bf1dcfa34
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-01 10:38:46 -0700

    new script

---
+++ lab_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Add /etc/hosts entries
+echo "10.0.2.100 server-1\n10.0.2.101 server-2\n10.0.2.102 server-3\n10.0.2.103 server-4" >> /etc/hosts
+
+#Add cloud_user ssh keys for passwordless ssh
+if [ "$HOSTNAME" = server-1 ]; then
+ssh-keygen -t rsa -N "" /home/cloud_user/.ssh/id_rsa
+chmod 0400 /home/cloud_user/.ssh/id_rsa /home/cloud_user/.ssh/id_rsa.pub
+chown cloud_user:cloud_user /home/cloud_user/.ssh/id_rsa /home/cloud_user/.ssh/id_rsa.pub
+else
+echo "nothing to do"
+fi
+
+
+#Copy key to other systems.
+for i in {1..4}
+do
+sshpass -f /root/cloud_pass ssh-copy-id -i /home/cloud_user/.ssh/id_rsa.pub -o "StrictHostKeyChecking no" cloud_user@server-${i}
+scp /home/cloud_user/.ssh/id_rsa cloud_user@server-${i}:/home/cloud_user/.ssh/id_rsa
+done
+
+
