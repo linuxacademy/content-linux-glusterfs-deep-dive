5613ebc1c8a62a4260b6b83e26357c979c657160
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-03 15:08:10 -0700

    Adding setup with iptables

---
+++ lab_setup_with_iptables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Add /etc/hosts entries
+echo "10.0.2.101 server-1
+10.0.2.102 server-2
+10.0.2.103 server-3
+10.0.2.104 server-4" >> /etc/hosts
+
+#restart glusterd due to some intermittent issues on lab starting.
+systemctl --no-pager --no-block restart glusterd
+sleep 5
+
+if [ "$HOSTNAME" = server-1 ]; then
+gluster peer probe server-2
+gluster peer probe server-3
+gluster peer probe server-4
+else
+echo "nothing to do"
+fi
+
+/usr/bin/wget https://github.com/linuxacademy/content-linux-glusterfs-deep-dive/raw/main/iptables.sh -P /root/
+chmod +x /root/iptables.sh
+/root/iptables.sh
+sleep 10
