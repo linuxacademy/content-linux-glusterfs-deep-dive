389fa6a017b20bb5c20cc6ffbb0a742e1b2c9a5e
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-03 08:38:43 -0700

    adding port 443

--- iptables.sh
+++ iptables.sh
@@ -21,6 +21,8 @@ iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 
 #Default discard traffic
 iptables -P INPUT DROP
