ddb9647868b16ad7cf9315e9cbe2023356fd0e3a
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-03 12:48:57 -0700

    Adding IP tables to this script

--- lab_setup.sh
+++ lab_setup.sh
@@ -6,6 +6,10 @@ echo "10.0.2.101 server-1
 10.0.2.103 server-3
 10.0.2.104 server-4" >> /etc/hosts
 
+#restart glusterd due to some intermittent issues on lab starting.
+systemctl --no-pager --no-block restart glusterd
+sleep 5
+
 if [ "$HOSTNAME" = server-1 ]; then
 gluster peer probe server-2
 gluster peer probe server-3
@@ -14,4 +18,7 @@ else
 echo "nothing to do"
 fi
 
-/usr/bin/wget -qO - https://github.com/linuxacademy/content-linux-glusterfs-deep-dive/raw/main/iptables.sh | bash
+/usr/bin/wget https://github.com/linuxacademy/content-linux-glusterfs-deep-dive/raw/main/iptables.sh 
+chmod +X /root/iptables.sh
+/root/iptables.sh
+sleep 10
