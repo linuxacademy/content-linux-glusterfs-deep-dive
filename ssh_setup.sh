769f395b046a246bb5287fb7c3c45f62b31f4245
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-16 15:28:26 -0700

    restart ssh

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -6,4 +6,5 @@
 /usr/bin/sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
 /usr/bin/sed -i 's/#Banner none/Banner \/etc\/motd/g' /etc/ssh/sshd_config
 echo "Welcome to $HOSTNAME" > /etc/motd
-systemctl restart ssh
+systemctl --no-block --no-pager restart ssh
+touch /tmp/ssh_setup_done
