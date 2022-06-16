578b1503adad340808b3c4653534bccd8c90437a
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-16 14:48:30 -0700

    passwordless ssh setup for some labs

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -7,32 +7,3 @@
 /usr/bin/sed -i 's/#Banner none/Banner \/etc\/motd/g' /etc/ssh/sshd_config
 echo "Welcome to $HOSTNAME" > /etc/motd
 systemctl restart ssh
-
-if [ "$HOSTNAME" = server-1 ]; then
-sleep 10
-#Generate key
-ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
-ssh-keygen -b 2048 -t rsa -f /home/cloud_user/.ssh/id_rsa -q -N ""
-chmod 0400 /home/cloud_user/.ssh/id_rsa
-chown cloud_user:cloud_user /home/cloud_user/.ssh/id_rsa
-
-#copy keys to rest of the servers
-for host in {1..6}; do
-attempt=0
-while [ $attempt -le 10 ]; do
-ssh -o KbdInteractiveAuthentication=no -o BatchMode=yes setup@10.0.2.10${host} 2>&1 | grep server || test $? -eq 0 && break
-sleep 6
-let attempt=attempt+1
-done
-sshpass -f /root/cloud_pass ssh-copy-id -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa root@10.0.2.10${host}
-sshpass -f /root/cloud_pass ssh-copy-id -o StrictHostKeyChecking=no -i /home/cloud_user/.ssh/id_rsa cloud_user@10.0.2.10${host}
-scp /root/.ssh/id_rsa* 10.0.2.10${host}:/root/.ssh/
-scp /home/cloud_user/.ssh/id_rsa* 10.0.2.10${host}:/home/cloud_user/.ssh/
-done
-
-#add key locally
-#cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-else
-echo "nothing to do"
-fi
-
