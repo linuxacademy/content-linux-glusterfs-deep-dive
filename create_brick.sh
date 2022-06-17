d1825f95c4c4e68ca50bd143ab9e233c0fdc85ba
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-06-17 13:53:06 -0700

    adding fstab entry

--- create_brick.sh
+++ create_brick.sh
@@ -5,9 +5,11 @@
 sleep 2
 
 #Create Filesystem
-mkfs.xfs -i size=512 /dev/nvme1n1p1 
+mkfs.xfs -i size=512 /dev/nvme1n1p1
 
-#mount filesystem and create brick subdirectory
+#add to /etc/fstab, mount filesystem, and create brick subdirectory
+UUID=`blkid /dev/nvme1n1p1 | cut -d \" -f 2`
+echo "UUID=$UUID /gfs xfs rw,inode64,noatime,nouuid 1 2" >> /etc/fstab
 mkdir /gfs
-mount /dev/nvme1n1p1 /gfs
+mount /gfs
 mkdir /gfs/brick-1
