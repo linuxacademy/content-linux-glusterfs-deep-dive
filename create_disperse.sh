7bd76d3078b4b5da744938ae8eb7253a8a288e18
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-07-07 16:02:09 -0700

    changed volume name

--- create_disperse.sh
+++ create_disperse.sh
@@ -14,6 +14,6 @@ done
 done
 
 #Enough attempts to validate brick, just create the volume win or lose.
-gluster volume create ganesha_vol server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1
+gluster volume create gfs_vol server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1
 
 touch /tmp/disperse_done
