2661dc84201a991fd0cb6fe37d6ef5444230f84a
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-07-07 12:20:43 -0700

    Ehh, it might work sort of

--- create_disperse.sh
+++ create_disperse.sh
@@ -4,4 +4,14 @@ until [ -f /tmp/brick_done ];
         do
           sleep 3
         done
-gluster volume create ganesha_vol disperse 4 redundancy 1 server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1 
+for host in {1..4}; do
+attempt=0
+while [ $attempt -le 10 ]; do
+ssh -o KbdInteractiveAuthentication=no -o BatchMode=yes 10.0.2.10${host} 2>&1 ls /tmp/brick_done || test $? -eq 0 && break
+sleep 6
+let attempt=attempt+1
+done
+done
+
+#Enough attempts to validate brick, just create the volume win or lose.
+gluster volume create ganesha_vol server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1
