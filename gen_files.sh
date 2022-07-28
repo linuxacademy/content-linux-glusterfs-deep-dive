e3bae38cf7e34d0ab51ed08df027da273e878519
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-07-28 07:56:46 -0700

    add wait

--- gen_files.sh
+++ gen_files.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# generate a number of files with random sizes in a range
 
+#wait for volume to be created
+until [ -f /tmp/disperse_done ] ;
+do
+	sleep 5
+done
+
+#start the GlusterFS volume
+gluster volume start gfs_vol
+
+#mount the volume locally
+mount -t glusterfs server-1:/gfs_vol /mnt
+
+#generate a number of files with random sizes in a range
 min=15      # min size (MB)
 max=50     # max size (MB)
 nofiles=150 # number of files
 
 for i in `eval echo {1..$nofiles}`
 do
-    dd bs=1M count=$(($RANDOM%max + $min)) if=/dev/urandom of=/mnt/file-$i  &
+    dd bs=1M count=$(($RANDOM%max + $min)) if=/dev/urandom of=/mnt/file-$i
 done
+
+touch /tmp/files_done
