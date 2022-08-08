e93b632e36cacdca89462437139a8caa3fe1e6f8
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-08-08 16:41:39 -0700

    fixing apt-get

--- testing/linstor_install.sh
+++ testing/linstor_install.sh
@@ -6,10 +6,10 @@ apt-get update
 
 #install drbd and linstor
 export DEBIAN_FRONTEND=noninteractive
-apt-install -y drbd-dkms drbd-utils lvm2 linstor-satellite linstor-client
+apt-get install -y drbd-dkms drbd-utils lvm2 linstor-satellite linstor-client
 if [ "$HOSTNAME" = server-1 ]; then
 	do
-		apt-install -y linstor-controller
+		apt-get install -y linstor-controller
 	done
 else
 echo "Not server-1"
