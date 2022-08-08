91d30ab7b36460668e440958f31d715918a3cf33
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-08-08 15:30:25 -0700

    test update

--- testing/test_setup.sh
+++ testing/test_setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Add /etc/hosts entries
+echo "10.0.2.101 server-1
+10.0.2.102 server-2
+10.0.2.103 server-3
+10.0.2.104 server-4
+10.0.2.105 server-5
+10.0.2.106 server-6" >> /etc/hosts
+
 #Install cloudformation helper scripts
 mkdir -p /opt/aws/bin
 /usr/bin/wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
