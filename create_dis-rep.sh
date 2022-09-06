48b5ee14bb72328c02802309a7208bd4ab4c638a
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-09-06 10:08:00 -0700

    new volume create script

---
+++ create_dis-rep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+until [ -f /tmp/brick_done ];
+        do
+          sleep 3
+        done
+for host in {1..6}; do
+attempt=0
+while [ $attempt -le 10 ]; do
+ssh -o KbdInteractiveAuthentication=no -o BatchMode=yes 10.0.2.10${host} 2>&1 ls /tmp/brick_done || test $? -eq 0 && break
+sleep 6
+let attempt=attempt+1
+done
+done
+
+#Enough attempts to validate brick, just create the volume win or lose.
+gluster volume create gfs_vol replica 2 server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1 server-5:/gfs/brick-1 server-6:/gfs/brick-1
+
+touch /tmp/disp_rep_done
