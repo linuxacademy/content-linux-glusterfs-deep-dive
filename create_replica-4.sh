ae2ebb61c788dd3272fbec7a8ce86c6e6fd25c87
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-09-23 10:47:24 -0700

    adding start for volume

--- create_replica-4.sh
+++ create_replica-4.sh
@@ -16,4 +16,7 @@ done
 #Enough attempts to validate brick, just create the volume win or lose.
 gluster volume create gfs_vol replica 4 server-1:/gfs/brick-1 server-2:/gfs/brick-1 server-3:/gfs/brick-1 server-4:/gfs/brick-1
 
+sleep 3
+gluster volume start gfs_vol
+
 touch /tmp/replica4_done
