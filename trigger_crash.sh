3e36ab356314f3158bde1b993029f3f919f4fb40
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2022-09-27 10:13:07 -0700

    adding a volume check

--- trigger_crash.sh
+++ trigger_crash.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+status=5
+while [ $status -ne 0 ]
+ do
+	 echo "in the loop"
+	 sleep 5
+	 check=$(gluster volume info | grep -i started)
+	 status=$?
+done
+
 ssh server-4 -q screen -d -m /root/crash.sh
